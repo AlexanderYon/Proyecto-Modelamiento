fc8c86b730ae009b684b76e81232f5096dac5a30
benjamin <benjamin.pozo2301@alumnos.ubiobio.cl>
2024-12-09 13:13:09 -0300

    se añade persistencia y se unifican los controladores en uno solo

---
+++ src/main/java/com/controlador/ControladorSistema.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.controlador;
+
+import com.modelo.Equipo;
+import com.modelo.Persona;
+import com.modelo.Rut;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import modelo.Prestamo;
+
+/**
+ *
+ * @author benja
+ */
+public class ControladorSistema {
+    private ArrayList<Equipo> listaEquipos;
+    private ArrayList<Prestamo> listaPrestamos;
+    private ArrayList<Persona> listaPersonas;
+    private static ControladorSistema INSTANCE = null;
+    private Persona encargado; // El encargado actual del laboratorio
+    
+    private ControladorSistema(){
+        listaEquipos = new ArrayList<>();
+        listaPrestamos = new ArrayList<>();
+        listaPersonas = new ArrayList<>();
+    }
+    public static ControladorSistema getInstance(){
+        if (INSTANCE==null){
+            INSTANCE=new ControladorSistema();
+            return INSTANCE;
+        }
+        return INSTANCE;
+    }
+    
+    public static boolean registrarEquipo(String id, String nombre, String descripcion){
+        
+        // REEMPLAZAR LUEGO
+        System.out.println("Imprimiendo datos recibidos para registro de equipo");
+        System.out.println(id + " - " + nombre + " - " + descripcion);
+        return true; // Se envía true si salio todo bien. Si hay algun error se envía una excepción
+    }
+    
+    public static boolean eliminarEquipo(String id){
+        System.out.println("Imprimiendo datos recibidos para registro de equipo");
+        System.out.println(id);
+        return true;
+    }
+    public static void registrarUsuario(String nombre, String rut, String fechaNacimiento, String nroTelefono){
+       
+    }
+    
+    public void guardarDatosSistema() throws IOException {
+        FileOutputStream fos = new FileOutputStream("Datos.obj");
+        ObjectOutputStream datos = new ObjectOutputStream(fos);
+        datos.writeObject(this);
+        datos.close();
+    }
+    public void recuperarDatosSistema() throws IOException, ClassNotFoundException{
+        FileInputStream fis = new FileInputStream("Datos.obj");
+        ObjectInputStream datos = new ObjectInputStream(fis);
+        INSTANCE = (ControladorSistema)datos.readObject();
+        datos.close();
+    }
+
+}
